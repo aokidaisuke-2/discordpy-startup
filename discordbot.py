0092a86ec2af214bb33d03a7c1730c2618a39c5f
aokidaisuke-2 <69669903+aokidaisuke-2@users.noreply.github.com>
2020-08-14 17:32:45 +0900

    update

--- discordbot.py
+++ discordbot.py
@@ -17,6 +17,20 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+    
+    
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("はろー"):
+        m = "こんにちは、" + message.author.name + "さん"
+        await client.send_message(message.channel,m)
 
 
 bot.run(token)
