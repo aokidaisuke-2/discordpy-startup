3e754cb26b2ad20dfd5aa53722c976a580a3f828
aokidaisuke-2 <69669903+aokidaisuke-2@users.noreply.github.com>
2020-08-14 17:25:02 +0900

    client

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,9 @@
 from discord.ext import commands
 import os
 import traceback
+import asyncio
+
+client = discord.Client()
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
